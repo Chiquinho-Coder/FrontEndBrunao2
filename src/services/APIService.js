c2fde478602dcb365f7f856589fe2f5849d6eb4e
Paulo Cesar <paulocesardearaujopaulo@gmail.com>
2024-11-06 19:40:50 -0300

    porta nomal

--- src/services/APIService.js
+++ src/services/APIService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = process.env.REACT_APP_API_URL2;
+const API_URL = process.env.REACT_APP_API_URL;
 
 export async function loginUser(credenciais) {
   try {
